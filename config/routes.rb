c18469d8688673667e5a6e71ff83c4ad453b4c61
guillermo haas-thompson <messenger@haasthompson.com>
2025-10-26 14:40:03 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ module Bookshelf
     root to: "home.index"
     get "/books", to: "books.index", as: :books
     get "/books/:id", to: "books.show", as: :show_book
-    get "/books/new", to: "books.new"
+    get "/books/new", to: "books.new", as: :new_book
     post "/books", to: "books.create", as: :create_book
     get "/books/:id/edit", to: "books.edit", as: :edit_book
     patch "/books/:id", to: "books.update", as: :update_book
