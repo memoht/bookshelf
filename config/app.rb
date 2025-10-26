9f1e55752017444d55c1ce3c8fb0ee0291c30d96
guillermo haas-thompson <messenger@haasthompson.com>
2025-10-26 14:39:57 -0400

    config > allow google font source

--- config/app.rb
+++ config/app.rb
@@ -9,5 +9,7 @@ module Bookshelf
       secret: settings.session_secret,
       expire_after: 60*60*24*365
     }
+    # For both development and production
+    config.actions.content_security_policy[:font_src] += " https://fonts.gstatic.com"
   end
 end
