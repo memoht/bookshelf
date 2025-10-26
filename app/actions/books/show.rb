295fd4e4212c0f0dba0995f0d8bbfb86d2a891fd
guillermo haas-thompson <messenger@haasthompson.com>
2025-10-26 09:41:05 -0400

    make handle_not_found method app wide

--- app/actions/books/show.rb
+++ app/actions/books/show.rb
@@ -5,18 +5,8 @@ module Bookshelf
   module Actions
     module Books
       class Show < Bookshelf::Action
-        handle_exception ROM::TupleCountMismatchError => :handle_not_found
-
         def handle(request, response)
           response.render(view, id: request.params[:id])
-        end
-
-        private
-
-        def handle_not_found(request, response, exception)
-          response.status = 404
-          response.format = :html
-          response.body = "Not found"
         end
       end
     end
