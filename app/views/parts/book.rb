a2098b94522ac6c0b5c9479bf5d6411a91ef9b9c
guillermo haas-thompson <messenger@haasthompson.com>
2025-10-30 14:15:57 -0400

    basic part (needs work)

--- app/views/parts/book.rb
+++ app/views/parts/book.rb
@@ -1,12 +1,9 @@
 # app/views/parts/book.rb
-
 # auto_register: false
-
 module Bookshelf
   module Views
     module Parts
       class Book < Bookshelf::Views::Part
-
         include Hanami::View::Helpers::EscapeHelper
 
         def show_path
@@ -17,6 +14,20 @@ module Bookshelf
           context.routes.path(:edit_book, id: id)
         end
 
+        def formatted_isbn
+          return nil unless isbn
+
+          digits = isbn.to_s.gsub(/[^0-9xX]/, '')
+
+          case digits.length
+          when 10
+            "#{digits[0]}-#{digits[1..6]}-#{digits[7..8]}-#{digits[9]}"
+          when 13
+            "#{digits[0..2]}-#{digits[3]}-#{digits[4..8]}-#{digits[9..11]}-#{digits[12]}"
+          else
+            isbn
+          end
+        end
       end
     end
   end
